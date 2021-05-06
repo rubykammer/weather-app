364aab22fa70140786e56f69c378a40c5b84dbed
rubykammer <ruby.kammer@gmail.com>
2021-05-06 11:25:08 +0200

    Removed unit converter

--- index.js
+++ index.js
@@ -30,7 +30,57 @@ let time = document.querySelector("#time");
   
   time.innerHTML = `${day} ${month} ${date}, ${hours}:${minutes}, ${year}`;
 
+  function formatDay(timestamp){
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
 
+    return days[day];
+
+  }
+
+
+function displayForecast(response) {
+ let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (0< index && index < 6) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="custom-day-column">
+            ${formatDay(forecastDay.dt)}
+            <br />
+            <img
+              src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+            />
+            <br />
+            <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}°</span> /
+            <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}°</span>
+          </div>
+
+  `;
+  }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+
+
+}
+
+function getForecast(coordinates){
+  console.log(coordinates);
+  let apiKey = "1fc3284d9435bcbb2a6f01ef23cf7980";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayWeather(response) {
 document.querySelector("#todayTemperaturePeak").innerHTML = Math.round(response.data.main.temp);
@@ -39,8 +89,16 @@ document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed)
 console.debug(response);
 let current  = response.data.weather[0].main;
 
- let h6 = document.querySelector("#currently");
+let h6 = document.querySelector("#currently");
  h6.innerHTML = `Current weather in ${response.data.name} is best described as "${current}"`;
+ 
+ let iconElement = document.querySelector("#current-weather");
+ iconElement.setAttribute(
+   "src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+ );
+
+ getForecast(response.data.coord);
+ 
 }
 
 function searchCity(city){
@@ -63,33 +121,9 @@ let city = document.querySelector("#cityForm");
 city.addEventListener("submit", showCity);
 
 
-function convertCToF(celsius) {
-  return (celsius * 9) / 5 + 32;
-
-} 
-function convertFToC(fahrenheit) {
-  return (fahrenheit - 32) * 5 / 9;
-
-}
-function changeTemperatureUnit(event) {
-let highest = document.querySelector("#todayTemperaturePeak");
-
-let newTemperature;
-
-if (event.target.checked) {
-  newTemperature = convertCToF(highest.innerHTML);
-
-} else {
-  newTemperature = convertFToC(highest.innerHTML);
-
-}
-highest.innerHTML = Math.round(newTemperature);
 
 
-}
 
-let toggle = document.querySelector("#flexSwitchCheckChecked");
-toggle.addEventListener("change", changeTemperatureUnit);
 
 function searchLocation(position) {
  let apiKey = "1fc3284d9435bcbb2a6f01ef23cf7980";
@@ -108,3 +142,4 @@ currentLocationButton.addEventListener("click", getCurrentLocation);
 
 
 searchCity("Berlin");
+displayForecast();
