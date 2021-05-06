8c6441660a7c197a4693083f2ad92f93a9585a95
rubykammer <ruby.kammer@gmail.com>
2021-05-06 17:13:15 +0200

    Removed dead code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let time = document.querySelector("#time");
 
       let date = now.getDate();
       let hours = now.getHours();
-      let minutes = now.getMinutes();
+      let minutes = now.getMinutes() < 10 ? "0"+ now.getMinutes(): now.getMinutes() ;
       let year = now.getFullYear();
 
       let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
